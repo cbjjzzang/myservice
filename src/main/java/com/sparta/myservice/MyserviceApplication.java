2ae2258fab9c56921644e889d397ca1d07fcee8e
Bongjin Choi <cbjjzzang@naver.com>
2022-03-24 17:00:00 +0900

    EC2 시간설정 추가

--- src/main/java/com/sparta/myservice/MyserviceApplication.java
+++ src/main/java/com/sparta/myservice/MyserviceApplication.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class MyserviceApplication {
 
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyserviceApplication.class, args);
+
+
     }
 }
